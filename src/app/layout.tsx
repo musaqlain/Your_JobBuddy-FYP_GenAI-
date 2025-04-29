b978fd87bc8a6798ccb59ea589d7423e7ca323e1
Muhammad Saqlain <2mesaqlain@gmail.com>
2025-04-29 16:37:45 +0500

    final finishing

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,9 +3,9 @@ import { TopNavBar } from "components/TopNavBar";
 import { Analytics } from "@vercel/analytics/react";
 
 export const metadata = {
-  title: "OpenResume - Free Open-source Resume Builder and Parser",
+  title: "UE Resume Builder and Mock Interview app",
   description:
-    "OpenResume is a free, open-source, and powerful resume builder that allows anyone to create a modern professional resume in 3 simple steps. For those who have an existing resume, OpenResume also provides a resume parser to help test and confirm its ATS readability.",
+    "UE resume builder and Mock interview is a free, open-source, and powerful resume builder that allows anyone to create a modern professional resume in 3 simple steps.",
 };
 
 export default function RootLayout({
